4f7e50835ff9d5a303a2f2bab076f35018efa7b7
Eoin O'Brien <iameoinobrien@gmail.com>
2015-12-29 23:19:15 +0000

    Updated

---
+++ station-details.js
@@ -0,0 +1,709 @@
+[
+{
+  "name"        : "Fenian Street",
+  "number"      : "63",
+  "lat"         : 53.341428,
+  "lng"         : -6.24672,
+  "credit-card" : false
+},
+{
+  "name"        : "City Quay",
+  "number"      : "99",
+  "lat"         : 53.346637,
+  "lng"         : -6.246154,
+  "credit-card" : false
+},
+{
+  "name"        : "Fitzwilliam Square East",
+  "number"      : "89",
+  "lat"         : 53.335211,
+  "lng"         : -6.2509,
+  "credit-card" : true
+},
+{
+  "name"        : "Brookfield Road",
+  "number"      : "84",
+  "lat"         : 53.339005,
+  "lng"         : -6.300217,
+  "credit-card" : false
+},
+{
+  "name"        : "Emmet Road",
+  "number"      : "83",
+  "lat"         : 53.340714,
+  "lng"         : -6.308191,
+  "credit-card" : false
+},
+{
+  "name"        : "Rothe Abbey",
+  "number"      : "85",
+  "lat"         : 53.338776,
+  "lng"         : -6.30395,
+  "credit-card" : true
+},
+{
+  "name"        : "King Street North",
+  "number"      : "101",
+  "lat"         : 53.350291,
+  "lng"         : -6.273507,
+  "credit-card" : false
+},
+{
+  "name"        : "Western Way",
+  "number"      : "102",
+  "lat"         : 53.354929,
+  "lng"         : -6.269425,
+  "credit-card" : false
+},
+{
+  "name"        : "Parkgate Street",
+  "number"      : "86",
+  "lat"         : 53.347972,
+  "lng"         : -6.291804,
+  "credit-card" : true
+},
+{
+  "name"        : "Heuston Bridge (South)",
+  "number"      : "100",
+  "lat"         : 53.347106,
+  "lng"         : -6.292041,
+  "credit-card" : false
+},
+{
+  "name"        : "Merrion Square East",
+  "number"      : "25",
+  "lat"         : 53.339434,
+  "lng"         : -6.246548,
+  "credit-card" : false
+},
+{
+  "name"        : "Merrion Square West",
+  "number"      : "26",
+  "lat"         : 53.339764,
+  "lng"         : -6.251988,
+  "credit-card" : true
+},
+{
+  "name"        : "Molesworth Street",
+  "number"      : "27",
+  "lat"         : 53.341288,
+  "lng"         : -6.258117,
+  "credit-card" : false
+},
+{
+  "name"        : "Mountjoy Square West",
+  "number"      : "28",
+  "lat"         : 53.356299,
+  "lng"         : -6.258586,
+  "credit-card" : false
+},
+{
+  "name"        : "Ormond Quay Upper",
+  "number"      : "29",
+  "lat"         : 53.346057,
+  "lng"         : -6.268001,
+  "credit-card" : true
+},
+{
+  "name"        : "Parnell Square North",
+  "number"      : "30",
+  "lat"         : 53.353462,
+  "lng"         : -6.265305,
+  "credit-card" : true
+},
+{
+  "name"        : "Bolton Street",
+  "number"      : "3",
+  "lat"         : 53.351182,
+  "lng"         : -6.269859,
+  "credit-card" : false
+},
+{
+  "name"        : "Cathal Brugha Street",
+  "number"      : "24",
+  "lat"         : 53.352149,
+  "lng"         : -6.260533,
+  "credit-card" : false
+},
+{
+  "name"        : "Parnell Street",
+  "number"      : "31",
+  "lat"         : 53.350929,
+  "lng"         : -6.265125,
+  "credit-card" : false
+},
+{
+  "name"        : "Pearse Street",
+  "number"      : "32",
+  "lat"         : 53.344304,
+  "lng"         : -6.250427,
+  "credit-card" : true
+},
+{
+  "name"        : "Greek Street",
+  "number"      : "4",
+  "lat"         : 53.346874,
+  "lng"         : -6.272976,
+  "credit-card" : false
+},
+{
+  "name"        : "Charlemont Street",
+  "number"      : "5",
+  "lat"         : 53.330662,
+  "lng"         : -6.260177,
+  "credit-card" : false
+},
+{
+  "name"        : "High Street",
+  "number"      : "7",
+  "lat"         : 53.343565,
+  "lng"         : -6.275071,
+  "credit-card" : true
+},
+{
+  "name"        : "Portobello Harbour",
+  "number"      : "34",
+  "lat"         : 53.330362,
+  "lng"         : -6.265163,
+  "credit-card" : false
+},
+{
+  "name"        : "Smithfield",
+  "number"      : "35",
+  "lat"         : 53.347692,
+  "lng"         : -6.278214,
+  "credit-card" : false
+},
+{
+  "name"        : "Custom House Quay",
+  "number"      : "8",
+  "lat"         : 53.347884,
+  "lng"         : -6.248048,
+  "credit-card" : false
+},
+{
+  "name"        : "St. Stephen's Green East",
+  "number"      : "36",
+  "lat"         : 53.337824,
+  "lng"         : -6.256035,
+  "credit-card" : true
+},
+{
+  "name"        : "Exchequer Street",
+  "number"      : "9",
+  "lat"         : 53.343034,
+  "lng"         : -6.263578,
+  "credit-card" : false
+},
+{
+  "name"        : "Dame Street",
+  "number"      : "10",
+  "lat"         : 53.344007,
+  "lng"         : -6.266802,
+  "credit-card" : true
+},
+{
+  "name"        : "Earlsfort Terrace",
+  "number"      : "11",
+  "lat"         : 53.334019,
+  "lng"         : -6.258371,
+  "credit-card" : false
+},
+{
+  "name"        : "Eccles Street",
+  "number"      : "12",
+  "lat"         : 53.359246,
+  "lng"         : -6.269779,
+  "credit-card" : false
+},
+{
+  "name"        : "Fitzwilliam Square West",
+  "number"      : "13",
+  "lat"         : 53.336074,
+  "lng"         : -6.252825,
+  "credit-card" : false
+},
+{
+  "name"        : "Fownes Street Upper",
+  "number"      : "14",
+  "lat"         : 53.344603,
+  "lng"         : -6.263371,
+  "credit-card" : false
+},
+{
+  "name"        : "St. Stephen's Green South",
+  "number"      : "37",
+  "lat"         : 53.337494,
+  "lng"         : -6.26199,
+  "credit-card" : true
+},
+{
+  "name"        : "Talbot Street",
+  "number"      : "38",
+  "lat"         : 53.350974,
+  "lng"         : -6.25294,
+  "credit-card" : false
+},
+{
+  "name"        : "Georges Quay",
+  "number"      : "16",
+  "lat"         : 53.347508,
+  "lng"         : -6.252192,
+  "credit-card" : false
+},
+{
+  "name"        : "Wilton Terrace",
+  "number"      : "39",
+  "lat"         : 53.332383,
+  "lng"         : -6.252717,
+  "credit-card" : true
+},
+{
+  "name"        : "Jervis Street",
+  "number"      : "40",
+  "lat"         : 53.3483,
+  "lng"         : -6.266651,
+  "credit-card" : true
+},
+{
+  "name"        : "Smithfield North",
+  "number"      : "42",
+  "lat"         : 53.349562,
+  "lng"         : -6.278198,
+  "credit-card" : true
+},
+{
+  "name"        : "Mount Street Lower",
+  "number"      : "56",
+  "lat"         : 53.33796,
+  "lng"         : -6.24153,
+  "credit-card" : false
+},
+{
+  "name"        : "Eccles Street East",
+  "number"      : "79",
+  "lat"         : 53.358115,
+  "lng"         : -6.265601,
+  "credit-card" : false
+},
+{
+  "name"        : "Portobello Road",
+  "number"      : "43",
+  "lat"         : 53.330091,
+  "lng"         : -6.268044,
+  "credit-card" : false
+},
+{
+  "name"        : "Hardwicke Place",
+  "number"      : "61",
+  "lat"         : 53.357043,
+  "lng"         : -6.263232,
+  "credit-card" : true
+},
+{
+  "name"        : "Upper Sherrard Street",
+  "number"      : "44",
+  "lat"         : 53.358437,
+  "lng"         : -6.260641,
+  "credit-card" : false
+},
+{
+  "name"        : "Lime Street",
+  "number"      : "62",
+  "lat"         : 53.346026,
+  "lng"         : -6.243576,
+  "credit-card" : false
+},
+{
+  "name"        : "New Central Bank",
+  "number"      : "66",
+  "lat"         : 53.347122,
+  "lng"         : -6.234749,
+  "credit-card" : false
+},
+{
+  "name"        : "Deverell Place",
+  "number"      : "45",
+  "lat"         : 53.351464,
+  "lng"         : -6.255265,
+  "credit-card" : false
+},
+{
+  "name"        : "James Street East",
+  "number"      : "20",
+  "lat"         : 53.336485,
+  "lng"         : -6.248174,
+  "credit-card" : false
+},
+{
+  "name"        : "Strand Street Great",
+  "number"      : "46",
+  "lat"         : 53.347082,
+  "lng"         : -6.264192,
+  "credit-card" : true
+},
+{
+  "name"        : "Herbert Street",
+  "number"      : "47",
+  "lat"         : 53.335742,
+  "lng"         : -6.24551,
+  "credit-card" : false
+},
+{
+  "name"        : "Excise Walk",
+  "number"      : "48",
+  "lat"         : 53.347777,
+  "lng"         : -6.244239,
+  "credit-card" : false
+},
+{
+  "name"        : "Custom House",
+  "number"      : "23",
+  "lat"         : 53.348279,
+  "lng"         : -6.254662,
+  "credit-card" : true
+},
+{
+  "name"        : "Leinster Street South",
+  "number"      : "21",
+  "lat"         : 53.34218,
+  "lng"         : -6.254485,
+  "credit-card" : false
+},
+{
+  "name"        : "Townsend Street",
+  "number"      : "22",
+  "lat"         : 53.345922,
+  "lng"         : -6.254614,
+  "credit-card" : false
+},
+{
+  "name"        : "Heuston Station (Central)",
+  "number"      : "93",
+  "lat"         : 53.346603,
+  "lng"         : -6.296924,
+  "credit-card" : false
+},
+{
+  "name"        : "Newman House",
+  "number"      : "53",
+  "lat"         : 53.337132,
+  "lng"         : -6.26059,
+  "credit-card" : false
+},
+{
+  "name"        : "Sandwith Street",
+  "number"      : "64",
+  "lat"         : 53.345203,
+  "lng"         : -6.247163,
+  "credit-card" : true
+},
+{
+  "name"        : "Sir Patrick's Dun",
+  "number"      : "58",
+  "lat"         : 53.339218,
+  "lng"         : -6.240642,
+  "credit-card" : true
+},
+{
+  "name"        : "St James Hospital (Luas)",
+  "number"      : "80",
+  "lat"         : 53.341359,
+  "lng"         : -6.292951,
+  "credit-card" : false
+},
+{
+  "name"        : "John Street West",
+  "number"      : "72",
+  "lat"         : 53.343105,
+  "lng"         : -6.277167,
+  "credit-card" : false
+},
+{
+  "name"        : "Kevin Street",
+  "number"      : "71",
+  "lat"         : 53.337757,
+  "lng"         : -6.267699,
+  "credit-card" : true
+},
+{
+  "name"        : "Hanover Quay",
+  "number"      : "68",
+  "lat"         : 53.344115,
+  "lng"         : -6.237153,
+  "credit-card" : true
+},
+{
+  "name"        : "Guild Street",
+  "number"      : "49",
+  "lat"         : 53.347932,
+  "lng"         : -6.240928,
+  "credit-card" : false
+},
+{
+  "name"        : "Harcourt Terrace",
+  "number"      : "41",
+  "lat"         : 53.332763,
+  "lng"         : -6.257942,
+  "credit-card" : false
+},
+{
+  "name"        : "James Street",
+  "number"      : "75",
+  "lat"         : 53.343456,
+  "lng"         : -6.287409,
+  "credit-card" : true
+},
+{
+  "name"        : "Mater Hospital",
+  "number"      : "78",
+  "lat"         : 53.359967,
+  "lng"         : -6.264828,
+  "credit-card" : false
+},
+{
+  "name"        : "Wolfe Tone Street",
+  "number"      : "77",
+  "lat"         : 53.348875,
+  "lng"         : -6.267459,
+  "credit-card" : false
+},
+{
+  "name"        : "Barrow Street",
+  "number"      : "70",
+  "lat"         : 53.341655,
+  "lng"         : -6.236198,
+  "credit-card" : true
+},
+{
+  "name"        : "Grand Canal Dock",
+  "number"      : "69",
+  "lat"         : 53.342638,
+  "lng"         : -6.238695,
+  "credit-card" : false
+},
+{
+  "name"        : "Francis Street",
+  "number"      : "73",
+  "lat"         : 53.342081,
+  "lng"         : -6.275233,
+  "credit-card" : true
+},
+{
+  "name"        : "The Point",
+  "number"      : "67",
+  "lat"         : 53.346867,
+  "lng"         : -6.230852,
+  "credit-card" : true
+},
+{
+  "name"        : "Blessington Street",
+  "number"      : "2",
+  "lat"         : 53.356769,
+  "lng"         : -6.26814,
+  "credit-card" : true
+},
+{
+  "name"        : "Collins Barracks Museum",
+  "number"      : "87",
+  "lat"         : 53.347477,
+  "lng"         : -6.28525,
+  "credit-card" : true
+},
+{
+  "name"        : "Blackhall Place",
+  "number"      : "88",
+  "lat"         : 53.3488,
+  "lng"         : -6.281637,
+  "credit-card" : false
+},
+{
+  "name"        : "Benson Street",
+  "number"      : "90",
+  "lat"         : 53.344153,
+  "lng"         : -6.233451,
+  "credit-card" : false
+},
+{
+  "name"        : "South Dock Road",
+  "number"      : "91",
+  "lat"         : 53.341833,
+  "lng"         : -6.231291,
+  "credit-card" : false
+},
+{
+  "name"        : "Heuston Bridge (North)",
+  "number"      : "92",
+  "lat"         : 53.347802,
+  "lng"         : -6.292432,
+  "credit-card" : false
+},
+{
+  "name"        : "Mount Brown",
+  "number"      : "82",
+  "lat"         : 53.341645,
+  "lng"         : -6.29719,
+  "credit-card" : false
+},
+{
+  "name"        : "Grattan Street",
+  "number"      : "57",
+  "lat"         : 53.339629,
+  "lng"         : -6.243778,
+  "credit-card" : false
+},
+{
+  "name"        : "Hatch Street",
+  "number"      : "55",
+  "lat"         : 53.33403,
+  "lng"         : -6.260714,
+  "credit-card" : false
+},
+{
+  "name"        : "Christchurch Place",
+  "number"      : "6",
+  "lat"         : 53.343368,
+  "lng"         : -6.27012,
+  "credit-card" : false
+},
+{
+  "name"        : "York Street West",
+  "number"      : "51",
+  "lat"         : 53.339334,
+  "lng"         : -6.264699,
+  "credit-card" : false
+},
+{
+  "name"        : "York Street East",
+  "number"      : "52",
+  "lat"         : 53.338755,
+  "lng"         : -6.262003,
+  "credit-card" : false
+},
+{
+  "name"        : "Clonmel Street",
+  "number"      : "54",
+  "lat"         : 53.336021,
+  "lng"         : -6.26298,
+  "credit-card" : false
+},
+{
+  "name"        : "Market Street South",
+  "number"      : "76",
+  "lat"         : 53.342296,
+  "lng"         : -6.287661,
+  "credit-card" : false
+},
+{
+  "name"        : "Convention Centre",
+  "number"      : "65",
+  "lat"         : 53.34744,
+  "lng"         : -6.238523,
+  "credit-card" : false
+},
+{
+  "name"        : "Oliver Bond Street",
+  "number"      : "74",
+  "lat"         : 53.343893,
+  "lng"         : -6.280531,
+  "credit-card" : false
+},
+{
+  "name"        : "Herbert Place",
+  "number"      : "19",
+  "lat"         : 53.334432,
+  "lng"         : -6.245575,
+  "credit-card" : false
+},
+{
+  "name"        : "Denmark Street Great",
+  "number"      : "59",
+  "lat"         : 53.35561,
+  "lng"         : -6.261397,
+  "credit-card" : false
+},
+{
+  "name"        : "North Circular Road",
+  "number"      : "60",
+  "lat"         : 53.359624,
+  "lng"         : -6.260348,
+  "credit-card" : true
+},
+{
+  "name"        : "Kilmainham Lane",
+  "number"      : "96",
+  "lat"         : 53.341805,
+  "lng"         : -6.305085,
+  "credit-card" : false
+},
+{
+  "name"        : "Royal Hospital",
+  "number"      : "95",
+  "lat"         : 53.343897,
+  "lng"         : -6.29706,
+  "credit-card" : true
+},
+{
+  "name"        : "Kilmainham Gaol",
+  "number"      : "97",
+  "lat"         : 53.342113,
+  "lng"         : -6.310015,
+  "credit-card" : true
+},
+{
+  "name"        : "Heuston Station (Car Park)",
+  "number"      : "94",
+  "lat"         : 53.346985,
+  "lng"         : -6.297804,
+  "credit-card" : false
+},
+{
+  "name"        : "Frederick Street South",
+  "number"      : "98",
+  "lat"         : 53.341515,
+  "lng"         : -6.256853,
+  "credit-card" : true
+},
+{
+  "name"        : "St. James Hospital (Central)",
+  "number"      : "81",
+  "lat"         : 53.339983,
+  "lng"         : -6.295594,
+  "credit-card" : false
+},
+{
+  "name"        : "Chatham Street",
+  "number"      : "1",
+  "lat"         : 53.340962,
+  "lng"         : -6.262287,
+  "credit-card" : false
+},
+{
+  "name"        : "Grantham Street",
+  "number"      : "18",
+  "lat"         : 53.334123,
+  "lng"         : -6.265436,
+  "credit-card" : true
+},
+{
+  "name"        : "Hardwicke Street",
+  "number"      : "15",
+  "lat"         : 53.355473,
+  "lng"         : -6.264423,
+  "credit-card" : false
+},
+{
+  "name"        : "Golden Lane",
+  "number"      : "17",
+  "lat"         : 53.340803,
+  "lng"         : -6.267732,
+  "credit-card" : false
+},
+{
+  "name"        : "Princes Street / O'Connell Street",
+  "number"      : "33",
+  "lat"         : 53.349013,
+  "lng"         : -6.260311,
+  "credit-card" : true
+}
+]
